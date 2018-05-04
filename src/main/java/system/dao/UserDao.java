1cae362ed7060e921a1627d7ede9da94944d7cdb
Caernarvon <zvirusmail@gmail.com>
2018-05-04 21:39:01 +0300

    changed list's type and deleted already registered users

--- src/main/java/system/dao/UserDao.java
+++ src/main/java/system/dao/UserDao.java
@@ -4,15 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import system.model.User;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Repository
 public class UserDao {
-    private List<User> users = Arrays.asList(new User("admin", "admin"),
-            new User("user1", "user1"));
+    private List<User> users = new ArrayList<User>();
 
     public List<User> getUsers(){
         return users;
+    }
+
+    public void addUser(User user) {
+        users.add(user);
     }
 }
