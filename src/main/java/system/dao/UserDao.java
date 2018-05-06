c5a9587f46b120d74dbbb5309087bdce87d1297c
Caernarvon <zvirusmail@gmail.com>
2018-05-07 01:06:25 +0300

    added several new things, but they didn't work at the moment

--- src/main/java/system/dao/UserDao.java
+++ src/main/java/system/dao/UserDao.java
@@ -1,12 +1,26 @@
 package system.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import system.model.User;
+import javax.activation.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class UserDao {
+
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     private List<User> users = new ArrayList<User>();
 
     public List<User> getUsers(){
