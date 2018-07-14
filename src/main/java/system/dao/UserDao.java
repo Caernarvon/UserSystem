4f217b0e002b463da6a7eb0d900ac46ed25cd5ac
Caernarvon <zvirusmail@gmail.com>
2018-07-14 16:06:55 +0300

    added connection pool, class reconfigurated to work with database

--- src/main/java/system/dao/UserDao.java
+++ src/main/java/system/dao/UserDao.java
@@ -1,33 +1,74 @@
 package system.dao;
-
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import system.model.User;
 import javax.activation.DataSource;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.beans.PropertyVetoException;
+import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class UserDao {
 
+    //TODO Autowired database, work w. connection pool
 
     @Autowired
-    DataSource dataSource;
+    private static UserDao userDao;
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
+    private BasicDataSource dataSource;
+    private final String DRIVER = "org.postgresql.Driver";
+    private final String USER = "postgres";
+    private final String PASSWORD = "root";
+    private final String URL = "jdbc:postgresql://localhost:5432/users";
+
+    private BasicDataSource getDataSource() {
+        if (dataSource == null) {
+            synchronized (this) {
+                if (dataSource == null) {
+                    dataSource = new BasicDataSource();
+                    dataSource.setUrl(URL);
+                    dataSource.setDriverClassName(DRIVER);
+                    dataSource.setUsername(USER);
+                    dataSource.setPassword(PASSWORD);
+                }
+            }
+        }
+        return dataSource;
+    }
 
     private List<User> users = new ArrayList<User>();
 
-    public List<User> getUsers(){
+    public List<User> getUsers() throws SQLException {
+        try {
+            Connection connection = getDataSource().getConnection();
+            PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM public.\"Users\"");
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                User user = new User(rs.getString(1), rs.getString(2));
+                users.add(user);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         return users;
     }
 
     public void addUser(User user) {
-        users.add(user);
+        try {
+            Connection connection = getDataSource().getConnection();
+            PreparedStatement pstmt =
+                    connection.prepareStatement("INSERT INTO public.\"Users\"(username, password) VALUES (?,?)");
+            pstmt.setString(1, user.getName());
+            pstmt.setString(2, user.getPassword());
+            pstmt.executeUpdate();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
